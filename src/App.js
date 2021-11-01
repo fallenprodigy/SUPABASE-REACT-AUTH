46f045148bea54b3793541d874f8d2f5e31bb485
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2021-11-01 18:29:51 +0400

    supabase

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './index.css';
+import Auth from './Auth';
+import Account from './Account';
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function Home() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return <ChakraProvider>{!session ? <Auth /> : <Account key={session.user.id} session={session} />}</ChakraProvider>;
+}
